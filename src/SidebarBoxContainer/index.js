b228d9486fc80c1d7a4aa96cd9312dc295fcff2d
Ngoan Tran <ngoan98travinh@gmail.com>
2022-01-25 22:36:47 +0700

    fix sidebar expanded size

--- src/SidebarBoxContainer/index.js
+++ src/SidebarBoxContainer/index.js
@@ -1,57 +1,13 @@
 // @flow
 
-import React, { useState, memo } from "react"
-import Paper from "@material-ui/core/Paper"
+import React, { memo } from "react"
 import { makeStyles } from "@material-ui/core/styles"
-import ExpandIcon from "@material-ui/icons/ExpandMore"
-import IconButton from "@material-ui/core/IconButton"
-import Collapse from "@material-ui/core/Collapse"
-import { grey } from "@material-ui/core/colors"
-import classnames from "classnames"
-import useEventCallback from "use-event-callback"
 import SidebarBox from "react-material-workspace-layout/SidebarBox"
 
 const useStyles = makeStyles({
-  container: { margin: 8 },
-  header: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 8,
-    paddingLeft: 16,
-    paddingRight: 16,
-  },
-  title: {
-    fontSize: 14,
-    fontWeight: "bold",
-    flexGrow: 1,
-    paddingLeft: 8,
-    color: grey[800],
-    "& span": {
-      color: grey[600],
-      fontSize: 12,
-    },
-  },
-  expandButton: {
-    padding: 0,
-    width: 30,
-    height: 30,
-    "& .icon": {
-      marginTop: -6,
-      width: 20,
-      height: 20,
-      transition: "500ms transform",
-      "&.expanded": {
-        transform: "rotate(180deg)",
-      },
-    },
-  },
-  expandedContent: {
-    maxHeight: "100%",
-    overflowY: "auto",
-    "&.noScroll": {
-      overflowY: "visible",
-      overflow: "visible",
+  container: {
+    "& .panel > *": {
+      maxHeight: "100%",
     },
   },
 })
@@ -59,27 +15,16 @@ const useStyles = makeStyles({
 export const SidebarBoxContainer = ({
   icon,
   title,
-  subTitle,
   children,
-  noScroll = false,
-  expandedByDefault = false,
 }) => {
   const classes = useStyles()
-  const content = (
-    <div
-      className={classnames(classes.expandedContent, noScroll && "noScroll")}
-    >
-      {children}
-    </div>
-  )
-
-  const [expanded, changeExpanded] = useState(expandedByDefault)
-  const toggleExpanded = useEventCallback(() => changeExpanded(!expanded))
 
   return (
-    <SidebarBox icon={icon} title={title} style={{ backgroundColor: "red" }}>
-      {children}
-    </SidebarBox>
+    <div className={classes.container}>
+      <SidebarBox icon={icon} title={title}>
+        {children}
+      </SidebarBox>
+    </div>
   )
 }
 
